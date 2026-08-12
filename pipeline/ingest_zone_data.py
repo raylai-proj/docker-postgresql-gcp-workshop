b45005ba1ae689a8c09d869a10d8c0620336e9d8
Chun Juei (Ray) Lai <chunjueilai@gmail.com>
2026-08-12 17:13:00 +0800

    refactor: refactor pipeline and wrap in new function

--- pipeline/ingest_zone_data.py
+++ pipeline/ingest_zone_data.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[35]:
-
-
 import pandas as pd
 from sqlalchemy import create_engine
 # from tqdm.auto import tqdm
 
-
-# In[36]:
-
-
 DATA_SOURCE_PREFIX = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/"
 ZONE_DATA = "taxi_zone_lookup.csv"
 
+POSTGRES = "postgresql"
+PG_USER = "root"
+PG_PASS = "root"
+PG_DB = "ny_taxi"
+PG_HOST = "localhost"
+PG_PORT = "5432"
+INGEST_TABLE_NAME = "zones"
+
+def ingest_taxi_zone_data():
+    zone_df = pd.read_csv(DATA_SOURCE_PREFIX+ZONE_DATA)
+    engine = create_engine(f"{POSTGRES}://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB}")
+    zone_df.to_sql(name=INGEST_TABLE_NAME, con=engine, if_exists='replace')
+
+if __name__ == "__main__":
+    ingest_taxi_zone_data()
 
 # In[37]:
 
@@ -35,13 +43,12 @@ ZONE_DATA = "taxi_zone_lookup.csv"
 
 
 # check if pandas exist
-pd.__file__
+# pd.__file__
 
 
 # In[40]:
 
 
-zone_df = pd.read_csv(DATA_SOURCE_PREFIX+ZONE_DATA)
 
 
 # In[44]:
@@ -53,37 +60,28 @@ zone_df = pd.read_csv(DATA_SOURCE_PREFIX+ZONE_DATA)
 # In[42]:
 
 
-POSTGRES = "postgresql"
-PG_USER = "root"
-PG_PASS = "root"
-PG_DB = "ny_taxi"
-PG_HOST = "localhost"
-PG_PORT = "5432"
-INGEST_TABLE_NAME = "zones"
 
 
 # In[43]:
 
 
-engine = create_engine(f"{POSTGRES}://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB}")
 
 
 # In[45]:
 
-
-print(pd.io.sql.get_schema(zone_df, name=INGEST_TABLE_NAME, con=engine))
+# # preview taxi zone lookup table schema
+# print(pd.io.sql.get_schema(zone_df, name=INGEST_TABLE_NAME, con=engine))
 
 
 # In[47]:
 
 
-zone_df.shape
+# zone_df.shape
 
 
 # In[48]:
 
 
-zone_df.to_sql(name=INGEST_TABLE_NAME, con=engine, if_exists='replace')
 
 
 # In[ ]:
