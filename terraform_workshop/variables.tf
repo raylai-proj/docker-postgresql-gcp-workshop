f53fb851381726e167165c32edd47a131c7bb3ae
Chun Juei (Ray) Lai <chunjueilai@gmail.com>
2026-08-25 22:03:05 +0800

    chore: add gcs_bucket_name variable for gcs bucket resource

--- terraform_workshop/variables.tf
+++ terraform_workshop/variables.tf
@@ -2,3 +2,8 @@ variable "bq_dataset_name" {
     description = "My BigQuery Dataset Name"
     default = "demo_dataset"
 }
+
+variable "gcs_bucket_name" {
+    description = "My Storage Bucket Name"
+    default = "project-9c55cdb0-ce48-42d5-902-terra-bucket"
+}
