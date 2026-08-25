b48e15d4963b3fb6bd29c6958944e1a5b2cf5252
Chun Juei (Ray) Lai <chunjueilai@gmail.com>
2026-08-25 22:27:13 +0800

    chore: add variables: project, region, zone

--- terraform_workshop/variables.tf
+++ terraform_workshop/variables.tf
@@ -1,6 +1,24 @@
+variable "project" {
+  description = "Project"
+  default     = "project-9c55cdb0-ce48-42d5-902"
+
+}
+
 variable "location" {
   description = "Project Location"
   default     = "asia-east1"
+}
+
+variable "region" {
+  description = "Project Region"
+  default     = "asia-east1"
+
+}
+
+variable "zone" {
+  description = "Project Zone"
+  default     = "asia-east1-a"
+
 }
 
 variable "bq_dataset_name" {
