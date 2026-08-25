368f49a409e69e5933977bb0212193fafea034a2
Chun Juei (Ray) Lai <chunjueilai@gmail.com>
2026-08-25 17:56:11 +0800

    chore:add google provider for terraform init and gcp bucket for terraform plan

---
+++ terraform_workshop/main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project = "project-9c55cdb0-ce48-42d5-902"
+  region  = "asia-east1"
+  zone    = "asia-east1-a"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "project-9c55cdb0-ce48-42d5-902-terra-bucket"
+  location      = "asia-east1"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
