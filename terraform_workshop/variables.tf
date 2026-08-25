39e687594d719cbb6e8313c72fdc6f90df0b4b5a
Chun Juei (Ray) Lai <chunjueilai@gmail.com>
2026-08-25 22:07:45 +0800

    style: correct variable.tf format

--- terraform_workshop/variables.tf
+++ terraform_workshop/variables.tf
@@ -1,20 +1,20 @@
 variable "location" {
-    description = "Project Location"
-    default = "asia-east1"
+  description = "Project Location"
+  default     = "asia-east1"
 }
 
 variable "bq_dataset_name" {
-    description = "My BigQuery Dataset Name"
-    default = "demo_dataset"
+  description = "My BigQuery Dataset Name"
+  default     = "demo_dataset"
 }
 
 variable "gcs_bucket_name" {
-    description = "My Storage Bucket Name"
-    default = "project-9c55cdb0-ce48-42d5-902-terra-bucket"
+  description = "My Storage Bucket Name"
+  default     = "project-9c55cdb0-ce48-42d5-902-terra-bucket"
 }
 
 variable "gcs_storage_class" {
-    description = "Bucket Storage Class"
-    default = "STANDARD"
-  
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+
 }
