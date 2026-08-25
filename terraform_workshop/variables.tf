ee62ae5351ddf634c79633091308fa41708540fe
Chun Juei (Ray) Lai <chunjueilai@gmail.com>
2026-08-25 22:04:38 +0800

    chore: add gcs storage class variable

--- terraform_workshop/variables.tf
+++ terraform_workshop/variables.tf
@@ -7,3 +7,9 @@ variable "gcs_bucket_name" {
     description = "My Storage Bucket Name"
     default = "project-9c55cdb0-ce48-42d5-902-terra-bucket"
 }
+
+variable "gcs_storage_class" {
+    description = "Bucket Storage Class"
+    default = "STANDARD"
+  
+}
