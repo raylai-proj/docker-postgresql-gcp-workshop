58d1a1c76cdd2f300863b95d0ce6acc8eb6a8182
Chun Juei (Ray) Lai <chunjueilai@gmail.com>
2026-08-25 19:14:28 +0800

    chore: add demo_dataset as google bigqueryy dataset resource

--- terraform_workshop/main.tf
+++ terraform_workshop/main.tf
@@ -28,3 +28,8 @@ resource "google_storage_bucket" "demo-bucket" {
     }
   }
 }
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = "demo_dataset"
+  location   = "asia-east1"
+}
